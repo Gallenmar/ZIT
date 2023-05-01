ef8bf9b3b807a430782bb17fab4be8932d86b57b
Nikita Plotnikov 221RDB021 <67636595+Gallenmar@users.noreply.github.com>
2023-05-01 23:30:44 +0300

    Attempt to fix decoder
    Fixed tree size input/output but tree char frequency input is stillmessed up

--- Data.java
+++
@@ -1,6 +0,0 @@
-import java.util.ArrayList;
-
-public class Data {
-    static ArrayList<Character> theData = new ArrayList<>();
-    static ArrayList<Character> theDataHuffman = new ArrayList<>();
-}
